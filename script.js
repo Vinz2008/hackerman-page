af513c5f0bc61b7519bfd1cf9d158414cee06b7b
Vinz2008 <clubfunreading@gmail.com>
2021-12-11 09:09:08 +0100

    added styling and js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const words = ["hello","world"]
+const words = ["import" ," subprocess" ," </br>", "nb_of_times"," </br>"]
 pos = 0
- $("#text").keydown(function(){
-  $("#text").html(words[pos]);
+a = 0
+length = words.length;
+ $("body").keydown(function(){
+  if (a == 0) {
+    $("#text").html(words[pos]);
+    
+  }
+  else {
+  $("#text").html(document.getElementById("text").innerHTML +   words[pos]);
+  }
+  
   pos = pos + 1
+  a = a + 1
+  if (pos == length)
+    pos = 0
 });
