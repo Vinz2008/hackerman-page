461f0bcec72eabf9ebe826b40aeb288124a3304a
Vinz2008 <clubfunreading@gmail.com>
2021-12-12 18:38:21 +0100

    make longer the sample text and added a little part to the site

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-const words = ["import" ," subprocess" ," </br>", "nb_of_times"," </br>"]
+const start = ["import" ," subprocess" ," </br>", "import ", "os", " </br>"]
+const words = [/*"import" ," subprocess" ," </br>",*/ " </br>"," </br>","nb_of_times ","= ","int(","input(","))"," </br>","for ","i ","in ","range","(nb_of_times","):","<br>","&nbsp;&nbsp;&nbsp;&nbsp;","try",":"," </br>","&nbsp;&nbsp;&nbsp;&nbsp;","&nbsp;&nbsp;&nbsp;&nbsp;","print(","subprocess",".","check_output","([","'ping',"," 'google.com'","]))"," </br>","&nbsp;&nbsp;&nbsp;&nbsp;","except ","subprocess", ".", "Called", "Process", "Error ", "as ", "err", ":", " </br>" ,"&nbsp;&nbsp;&nbsp;&nbsp;","&nbsp;&nbsp;&nbsp;&nbsp;", "print(", "err)", " </br>"]
 pos = 0
 a = 0
 length = words.length;
  $("body").keydown(function(){
   if (a == 0) {
-    $("#text").html(words[pos]);
+    $("#text").html(start[pos]);
+  }
+  else {
+  if (a < 5 && a != 0)  {
+    $("#text").html(document.getElementById("text").innerHTML +   start[pos]);
     
   }
   else {
+    if (a == 5) {
+      pos = 0
+    }
+  
   $("#text").html(document.getElementById("text").innerHTML +   words[pos]);
   }
+}
   
   pos = pos + 1
   a = a + 1
