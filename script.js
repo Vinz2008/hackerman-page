4efb6fc36599704569d615c9b9bcc06bffe60f9b
Vinz2008 <clubfunreading@gmail.com>
2021-12-28 13:33:55 +0100

    added autoscroll

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+var scroll = setInterval(function(){ window.scrollBy(0,1000); }, 10);
 const start = ["import" ," subprocess" ," </br>", "import ", "os", " </br>"]
-const words = [/*"import" ," subprocess" ," </br>",*/ " </br>"," </br>","nb_of_times ","= ","int(","input(","))"," </br>","for ","i ","in ","range","(nb_of_times","):","<br>","&nbsp;&nbsp;&nbsp;&nbsp;","try",":"," </br>","&nbsp;&nbsp;&nbsp;&nbsp;","&nbsp;&nbsp;&nbsp;&nbsp;","print(","subprocess",".","check_output","([","'ping',"," 'google.com'","]))"," </br>","&nbsp;&nbsp;&nbsp;&nbsp;","except ","subprocess", ".", "Called", "Process", "Error ", "as ", "err", ":", " </br>" ,"&nbsp;&nbsp;&nbsp;&nbsp;","&nbsp;&nbsp;&nbsp;&nbsp;", "print(", "err)", " </br>"]
+const words = [/*"import" ," subprocess" ," </br>",*/ " </br>"," </br>","nb_of_times ","= ","int(","input(","))"," </br>","for ","i ","in ","range","(nb_of_times","):","<br>","&nbsp;&nbsp;&nbsp;&nbsp;","try",":"," </br>","&nbsp;&nbsp;&nbsp;&nbsp;","&nbsp;&nbsp;&nbsp;&nbsp;","print(","subprocess",".","check_output","([","'ping',"," 'google.com'","]))"," </br>","&nbsp;&nbsp;&nbsp;&nbsp;","except ","subprocess", ".", "Called", "Process", "Error ", "as ", "err", ":", " </br>" ,"&nbsp;&nbsp;&nbsp;&nbsp;","&nbsp;&nbsp;&nbsp;&nbsp;", "print(", "err)", " </br>", "confi","gure","_logging","(settings",".LOGGING_CONFIG",", settings",".LOGGING","</br>","&nbsp;&nbsp;&nbsp;&nbsp;","if ", "set_prefix:","</br>","&nbsp;&nbsp;&nbsp;&nbsp;","&nbsp;&nbsp;&nbsp;&nbsp;","set-script","_prefix(","</br>"]
 pos = 0
 a = 0
  $("body").keydown(function(){
@@ -22,8 +23,9 @@ a = 0
   
   pos = pos + 1
   a = a + 1
-  if (pos == length) {
+  if (pos == words.length-1) {
     pos = 0
+    console.log(words.length)
   }
   
 });
